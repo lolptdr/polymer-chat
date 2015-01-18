cda2b51df9cb2f3df02d2848c703fda2cd8b7a8e
Joseph Tingsanchali <charnwoot@gmail.com>
2015-01-18 12:33:11 -0600

    Add UUID functions; Correct querySelector method

--- js/application.js
+++ js/application.js
@@ -3,12 +3,26 @@
 
   var uuid, avatar, color, cat;
 
-  
-
-
+  // Assign a uuid made of a random cat and a random color
+  var randomColor = function() {   
+    var colors = ['navy', 'slate', 'olive', 'moss', 'chocolate', 'buttercup', 'maroon', 'cerise', 'plum', 'orchid'];   
+    return colors[(Math.random() * colors.length) >>> 0];
+  };
+  var randomCat = function() {   
+    var cats = ['tabby', 'bengal', 'persian', 'mainecoon', 'ragdoll', 'sphynx', 'siamese', 'korat', 'japanesebobtail', 'abyssinian', 'scottishfold'];           
+    return cats[(Math.random() * cats.length) >>> 0];        
+  }; 
+  color = randomColor();
+  cat = randomCat();
+  uuid = color + '-' + cat;
+  avatar = 'images/' + cat + '.jpg';
+  function showNewest() {
+    var chatDiv = document.querySelector('.chat-list');
+    chatDiv.scrollTop = chatDiv.scrollHeight;
+  }
 
   // Polymer UI and UX
-  var template = document.querySelect('template[is=auto-binding]');
+  var template = document.querySelector('template[is=auto-binding]');
   
   template.channel = 'polymer-chat';
   template.uuid = uuid;
