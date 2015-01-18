30fe063bf4f7ade0a6272ac256aead0513c4dcf4
Joseph Tingsanchali <charnwoot@gmail.com>
2015-01-18 12:19:21 -0600

    Add pubnub functions and ui/ux features in js

---
+++ js/application.js
@@ -0,0 +1,56 @@
+(function() {
+  'use strict';
+
+  var uuid, avatar, color, cat;
+
+  
+
+
+
+  // Polymer UI and UX
+  var template = document.querySelect('template[is=auto-binding]');
+  
+  template.channel = 'polymer-chat';
+  template.uuid = uuid;
+  template.avatar = avatar;
+  template.color = color;
+
+  template.items = [
+    {icon: 'cloud', title: 'PubNub'},
+    {icon: 'polymer', title: 'Polymer'},
+    {icon: 'favorite', title: 'Love'}
+  ];
+
+  template.checkKey = function(e) {
+    if(e.keyCode === 13 || e.charCode === 13) {
+        template.publish();
+    }
+  };
+
+  template.sendMyMessage = function(e) {
+    template.publish();
+  };
+
+  // PubNub Realtime Chat
+
+  template.publish = function() {
+    if(!template.input) return;
+
+    template.$.pub.message = {
+      uuid: uuid,
+      avatar: avatar,
+      color: color,
+      text: template.input,
+      timestamp: Date.now()
+    };
+
+    template.$.pub.publish();
+    template.input = '';
+  };
+
+  template.subscribeCallback = function(e) {
+    template.aync(showNewest);
+  };
+
+})();
+
